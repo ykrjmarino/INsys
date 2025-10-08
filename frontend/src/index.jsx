f6fb4cbf7ff8eadd98901f2407628f7f47d30a9c
YlaMarino <marinoyla333@gmail.com>
2025-10-08 14:52:54 +0800

    modify for live deployment

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -14,6 +14,17 @@ import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
+import './css/styles.css';
+import './css/register-ts.css';
+import './css/exam-create-t.css';
+import './css/home-t.css';
+import './css/home-s.css';
+import './css/score-exam-s.css';
+import './css/exam-instr-s.css';
+import './css/exam-ques-types-s.css';
+import './css/register-s.css';
+import './css/login.css';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
