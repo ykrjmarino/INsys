b1cf0e427923cc6ab8c15544ca9af48203c1949b
YlaMarino <marinoyla333@gmail.com>
2025-10-08 13:55:21 +0800

    redisClient.js modify for live deployment

--- backend/utils/redisClient.js
+++ backend/utils/redisClient.js
@@ -1,6 +1,23 @@
+// import { createClient } from 'redis';
+
+// const redisClient = createClient();
+
+// redisClient.on("error", (err) => {
+//   console.error("Redis Client Error: ", err);
+// });
+
+// await redisClient.connect();
+
+// export default redisClient;
+
+
+
 import { createClient } from 'redis';
 
-const redisClient = createClient();
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+  socket: { tls: true, rejectUnauthorized: false },
+});
 
 redisClient.on("error", (err) => {
   console.error("Redis Client Error: ", err);
