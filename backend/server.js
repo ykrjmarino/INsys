bf0c3b6be0266d827b61018153ee3ff64811b8d7
YlaMarino <marinoyla333@gmail.com>
2025-10-18 22:23:50 +0800

    deploy server.js removed path

--- backend/server.js
+++ backend/server.js
@@ -99,26 +99,12 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(
-//   cors({
-//     origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
-//     credentials: true,
-//   })
-// );
-
-app.use(cors({
-  origin: function (origin, callback) {
-    const allowed = [process.env.FRONTEND_URL, "http://localhost:5173"];
-    if (!origin || allowed.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));
-
-
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.use(
   session({
@@ -222,11 +208,6 @@ app.get("/api/exams/analytics/:examId", getExamAnalytics);
 app.get("/api/exams/student/:studentId/analytics/:examId", getStudentAnalytics);
 
 
-
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
 // ======================
 // 🔟 START SERVER
 // ======================
