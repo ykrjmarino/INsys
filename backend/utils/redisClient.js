df9ffcc7a05b98188ddbc81c9339c1d0297b3dc5
YlaMarino <marinoyla333@gmail.com>
2025-10-18 19:29:44 +0800

    deploy redisClient url

--- backend/utils/redisClient.js
+++ backend/utils/redisClient.js
@@ -2,7 +2,7 @@ import { createClient } from 'redis';
 import dotenv from "dotenv"
 dotenv.config();
 const redisClient = createClient({
-  url: process.env.LOCAL ? '' : 'redis://red-d3j4u8mmcj7s739ma92g:s2wMPEzbk1lGkhQ8xWFyqmvz27HwgJZN@red-d3j4u8mmcj7s739ma92g:6379'
+  url: process.env.LOCAL ? '' : 'redis://default:0dYFNg4q8RldJx3wrrrAXooB1blkCvLk@redis-12975.c261.us-east-1-4.ec2.redns.redis-cloud.com:12975'
 });
 
 redisClient.on("error", (err) => {
