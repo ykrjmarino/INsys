2a2e63f2802908013d8a2b8e04d561a3f9b13636
YlaMarino <marinoyla333@gmail.com>
2025-10-18 14:45:15 +0800

    deploy

--- backend/utils/nodemailer.js
+++ backend/utils/nodemailer.js
@@ -7,6 +7,7 @@ dotenv.config({ path: '../.env', quiet: true });
 export const sendUserEmail = async({email, token, context }) => {
   const transporter = nodemailer.createTransport({
     service:"gmail",
+    secure: true,
     auth: {
       user: process.env.NDM_USER,
       pass: process.env.NDM_PASSWORD
