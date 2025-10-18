2f339b814e14352d90b9ca36b959303114be2074
YlaMarino <marinoyla333@gmail.com>
2025-10-18 13:56:20 +0800

    deploy initial

--- backend/utils/redisClient.js
+++ backend/utils/redisClient.js
@@ -1,6 +1,8 @@
 import { createClient } from 'redis';
 
-const redisClient = createClient();
+const redisClient = createClient({
+  url: 'redis://red-d3j4u8mmcj7s739ma92g:s2wMPEzbk1lGkhQ8xWFyqmvz27HwgJZN@red-d3j4u8mmcj7s739ma92g:6379'
+});
 
 redisClient.on("error", (err) => {
   console.error("Redis Client Error: ", err);
