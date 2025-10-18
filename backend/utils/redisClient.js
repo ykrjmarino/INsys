47e9c7cfd8c3d9b4841fa9b3a546160ec079012f
YlaMarino <marinoyla333@gmail.com>
2025-10-18 21:34:48 +0800

    deploy forgot to commit one file

--- backend/utils/redisClient.js
+++ backend/utils/redisClient.js
@@ -2,7 +2,7 @@ import { createClient } from 'redis';
 import dotenv from "dotenv"
 dotenv.config();
 const redisClient = createClient({
-  url: process.env.LOCAL ? '' : 'redis://default:0dYFNg4q8RldJx3wrrrAXooB1blkCvLk@redis-12975.c261.us-east-1-4.ec2.redns.redis-cloud.com:12975'
+  url: process.env.LOCAL ? '' : process.env.REDIS_URL
 });
 
 redisClient.on("error", (err) => {
