13f5473db71fe4e51fdd2f82d84302ba2bc1de5a
YlaMarino <marinoyla333@gmail.com>
2025-10-18 14:28:37 +0800

    deploy baseURL changed to env VITE_API_BASE_FRONT_URL

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -3,6 +3,22 @@ import axiosLib  from 'axios';
 console.log("Base URL:", import.meta.env.VITE_API_BASE_URL);
 
 const axios = axiosLib .create({
+  baseURL: import.meta.env.VITE_API_BASE_FRONT_URL, //https://insys-front.onrender.com (.env)
+  withCredentials: true, 
+});
+console.log("Axios instance baseURL:", axios.defaults.baseURL);
+
+
+export default axios;
+
+
+//for localhost
+/*
+import axiosLib  from 'axios';
+
+console.log("Base URL:", import.meta.env.VITE_API_BASE_URL);
+
+const axios = axiosLib .create({
   baseURL: import.meta.env.VITE_API_BASE_URL, //http://localhost:3000/api (.env)
   withCredentials: true, 
 });
@@ -10,3 +26,4 @@ console.log("Axios instance baseURL:", axios.defaults.baseURL);
 
 
 export default axios;
+*/
